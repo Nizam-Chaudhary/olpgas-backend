dccc2ba5232aa42c8acb29c91c26826b2421d885
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2024-09-10 10:35:12 +0530

    update(config): added production config

--- config/config.js
+++ config/config.js
@@ -17,10 +17,11 @@ module.exports = {
 		dialect: 'mysql',
 	},
 	production: {
-		username: 'root',
-		password: null,
-		database: 'database_production',
-		host: '127.0.0.1',
-		dialect: 'mysql',
+		username: process.env.PG_USER,
+		password: process.env.PG_PASSWORD,
+		database: process.env.PG_DATABASE,
+		host: process.env.PG_HOST,
+		dialect: 'postgres',
+		dialectOptions: { ssl: { require: true } },
 	},
 }
